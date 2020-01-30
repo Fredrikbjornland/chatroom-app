48c975c8e461cd23583833306b7cc2a3b3db1b95
Fredrikbjornland <fredrik.bjornland@vikenfiber.no>
2020-01-30 16:11:02 +0100

    add client

---
+++ client/.netlify/build/precache-manifest.c8ed3f99f52f5a3b1dd009b2b22ec055.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "05b0bef87129173f23d6f53cb08e9d10",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9e4249319c3623ff5136",
+    "url": "/static/css/main.6a2a8a0f.chunk.css"
+  },
+  {
+    "revision": "b8cbe84e21d6912bae45",
+    "url": "/static/js/2.f72d79fc.chunk.js"
+  },
+  {
+    "revision": "e5fdd3c10a39c54f9723efcb1214ee6a",
+    "url": "/static/js/2.f72d79fc.chunk.js.LICENSE"
+  },
+  {
+    "revision": "9e4249319c3623ff5136",
+    "url": "/static/js/main.b0bdb186.chunk.js"
+  },
+  {
+    "revision": "5d93c6b2d15332364552",
+    "url": "/static/js/runtime-main.cea588d5.js"
+  }
+]);
